6b1d08bd8b6c67155118463328b1116320cb33d9
Abrenes <abremar.upo@gmail.com>
2019-11-28 14:33:43 +0000

    Added support for method GET /contacts

--- index.js
+++ index.js
@@ -37,7 +37,14 @@ app.get("/", (req, res) => {
 
 app.get(BASE_API_PATH + "/contacts", (req, res) => {
     console.log(Date() + " - GET /contacts");
-    res.send([]);
+    db.find({}, (err, contacts) => {
+        if (err) {
+            console.log(Date() + " - " + err);
+            res.sendStatus(500);
+        } else {
+            res.sendStatus(contacts);
+        }
+    });
 });
 
 app.post(BASE_API_PATH + "/contacts", (req, res) => {
